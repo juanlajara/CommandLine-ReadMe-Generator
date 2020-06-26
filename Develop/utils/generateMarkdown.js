d05527924e2b46f7f929be09a3eef8de8fb1773e
juanlajara <juanlajara001@gmail.com>
2020-06-26 01:29:06 -0500

    + adjusted to better meet request

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -21,28 +21,13 @@ ${data.installation}
 
 ${data.usage}
 
-
-// ## Credits
-
-// List your collaborators, if any, with links to their GitHub profiles.
-
-// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-// If you followed tutorials, include links to those here as well.
-
 ## License
 
 ${data.license}
 
-
----
-
 ## Badges
 
 ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
 
 ## Contributing
 
